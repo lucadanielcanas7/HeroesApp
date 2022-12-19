bce1f0f44f64398f9ab77545e6ad5d25914e3e29
Luca Daniel Canas <luca.daniel.canas@accenture.com>
2022-12-19 14:33:20 -0300

    Init project

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddComponent,
+    SearchComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }
